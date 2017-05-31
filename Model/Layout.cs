1b2fc25c80dfa1b3022428d042f7822c98bd2009
ElasticSea <info@elasticsea.com>
2017-05-31 13:58:35 +0200

    Adding layout model.

---
+++ Model/Layout.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WinTile.Model
+{
+    public class Layout
+    {
+        public List<WindowTile> windows = new List<WindowTile>();
+    }
+}
---
+++ Model/WindowTile.cs
@@ -0,0 +1,14 @@
+﻿namespace WinTile.Model
+{
+    public class WindowTile
+    {
+        public Rect tile;
+        private string hotkey;
+
+        public WindowTile(Rect tile, string hotkey = null)
+        {
+            this.tile = tile;
+            this.hotkey = hotkey;
+        }
+    }
+}
