b0dfccb5b465a52bb0c5c7b7550e5ad0037c7d93
ElasticSea <info@elasticsea.com>
2017-05-31 14:05:42 +0200

    Basic implementation.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,57 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using WinTile.Model;
 
 namespace WinTile
 {
     public partial class MainWindow : Window
     {
-        private List<Button> Windows = new List<Button>();
+        private ViewModel viewModel = new ViewModel();
+        private Map<WindowTile, ToggleButton> Windows = new Map<WindowTile, ToggleButton>();
 
         public MainWindow()
         {
+            viewModel.WindowAdded += AddWindow;
+            viewModel.WindowRemoved += removeWindow;
+            viewModel.WindowChanged += WindowChanged;
+
             InitializeComponent();
+
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                DataContext = viewModel;
+            }
+
+            Loaded += (sender, args) =>
+            {
+                viewModel.Load();
+            };
         }
 
         private void AddWindowButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            viewModel.AddWindow();
+        }
+
+        private void AddWindow(WindowTile window)
+        {
+
+            var button = new ToggleButton();
+            rescaleToggle(window, button);
+
+            Windows.Add(window, button);
+
+            button.Click += (sender, args) =>
+            {
+                if (sender == button && button.IsChecked == false)
+                {
+                    viewModel.Selected = null;
+                }
+                else
+                {
+                    // Deselect other buttons
+                    Windows.Values.ToList()
+                        .Where(b => b != button)
+                        .ForEach(b => b.IsChecked = false);
+
+                    viewModel.Selected = Windows.Reverse[button];
+                }
+            };
+
+            Canvas.Children.Add(button);
+        }
+
+        private void removeWindow(WindowTile window)
+        {
+            var toggle = Windows.Forward[window];
+            Canvas.Children.Remove(toggle);
+        }
+
+        private void rescaleToggle(WindowTile window, ToggleButton button)
+        {
+            var scaledWindow = window.tile.extend(Canvas.ActualWidth, Canvas.ActualHeight);
+            Canvas.SetLeft(button, scaledWindow.Left);
+            Canvas.SetTop(button, scaledWindow.Top);
+            button.Width = scaledWindow.Width;
+            button.Height = scaledWindow.Height;
+        }
+
+        private void WindowChanged(WindowTile window)
+        {
+            var toggle = CurrentToggle();
+            if(toggle!=null) rescaleToggle(window, toggle);
+        }
+
+        private ToggleButton CurrentToggle()
+        {
+            return Windows.Values.FirstOrDefault(b => b.IsChecked == true);
         }
 
         private void RemoveWindowButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
-
-        private void TogglePxPercentButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+            var toggle = CurrentToggle();
+            if (toggle != null) viewModel.removeWindow(Windows.Reverse[toggle]);
         }
 
         private void ApplyLayoutButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
-
-        private void XTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void YTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void WidthTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void HeightTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            throw new NotImplementedException();
+            viewModel.Save();
         }
     }
 }
---
+++ ViewModel.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using WinTile.Model;
+
+namespace WinTile
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event Action<WindowTile> WindowAdded = rect => { };
+        public event Action<WindowTile> WindowRemoved = rect => { };
+        public event Action<WindowTile> WindowChanged = rect => { };
+
+        public event PropertyChangedEventHandler PropertyChanged = (sender, args) => { };
+
+        private readonly Layout layout = JsonConvert.DeserializeObject<Layout>(Properties.Settings.Default.Layout ?? "{}") ?? new Layout();
+        private WindowTile selected;
+        private float _left = 0f;
+        private float _top = 0f;
+        private float _right = 0f;
+        private float _bottom = 0f;
+
+        internal void Load()
+        {
+            foreach (var windowTile in layout.windows)
+            {
+                WindowAdded(windowTile);
+            }
+        }
+
+        public float Left
+        {
+            get { return _left; }
+            set
+            {
+                if(value == _left) return;
+
+                _left = Math.Max(value, 0f);
+                Right = Math.Max(_left, _right);
+                TriggerTileChanged();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Left)));
+            }
+        }
+
+        private void TriggerTileChanged()
+        {
+            if (selected != null)
+            {
+                selected.tile.Left  = Left / 100;
+                selected.tile.Right = Right / 100;
+                selected.tile.Top = Top / 100;
+                selected.tile.Bottom = Bottom / 100;
+
+                WindowChanged(selected);
+            }
+        }
+
+        public float Top
+        {
+            get { return _top; }
+            set
+            {
+                if (value == _top) return;
+
+                _top = Math.Max(value, 0f);
+                Bottom = Math.Max(_top, _bottom);
+                TriggerTileChanged();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Top)));
+            }
+        }
+
+        public float Right
+        {
+            get { return _right; }
+            set
+            {
+                if (value == _right) return;
+
+                _right = Math.Min(value, 100f);
+                Left = Math.Min(_left, _right);
+                TriggerTileChanged();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Right)));
+            }
+        }
+
+        public float Bottom
+        {
+            get { return _bottom; }
+            set
+            {
+                if (value == _bottom) return;
+
+                _bottom = Math.Min(value, 100f);
+                Top = Math.Min(_top, _bottom);
+                TriggerTileChanged();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Bottom)));
+            }
+        }
+
+        internal WindowTile Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+
+                if (selected != null)
+                {
+                    _left = selected.tile.Left * 100;
+                    _right = selected.tile.Right * 100;
+                    _top = selected.tile.Top * 100;
+                    _bottom = selected.tile.Bottom * 100;
+
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Left)));
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Top)));
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Right)));
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Bottom)));
+                }
+            }
+        }
+
+        public void AddWindow()
+        {
+            var window = new WindowTile(new Rect(Left, Top, Right, Bottom) / 100f);
+            layout.windows.Add(window);
+            WindowAdded(window);
+        }
+
+        public void removeWindow(WindowTile window)
+        {
+            layout.windows.Remove(window);
+            WindowRemoved(window);
+        }
+
+        internal void Save()
+        {
+            Properties.Settings.Default.Layout = JsonConvert.SerializeObject(layout);
+            Properties.Settings.Default.Save();
+        }
+    }
+}
