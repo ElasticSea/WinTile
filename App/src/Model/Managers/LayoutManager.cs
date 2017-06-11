d5952799c152c5b53590abaeedbb63ab5ba83908
ElasticSea <info@elasticsea.com>
2017-06-11 15:25:55 +0200

    Ommit null values in json

--- App/src/Model/Managers/LayoutManager.cs
+++ App/src/Model/Managers/LayoutManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace App.Model.Managers
 {
@@ -8,9 +10,16 @@ namespace App.Model.Managers
     {
         public Layout Layout { get; private set; }
 
+        private readonly JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            Converters = new List<JsonConverter> { new StringEnumConverter() },
+            Formatting = Formatting.Indented
+        };
+
         public string Json
         {
-            get => JsonConvert.SerializeObject(Layout, Formatting.Indented);
+            get => JsonConvert.SerializeObject(Layout, settings);
             set
             {
                 try
